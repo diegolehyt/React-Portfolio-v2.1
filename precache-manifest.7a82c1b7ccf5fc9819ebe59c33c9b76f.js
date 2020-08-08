354bb4f9a35fd8f4e4459fdcb845f2a40d6752c9
Diego Lehyt <diegolehy00@gmail.com>
2020-08-08 11:35:56 -0400

    Updates

---
+++ precache-manifest.7a82c1b7ccf5fc9819ebe59c33c9b76f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15396b26d522fa477a40da0571e8ab56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "884268b2431702620e6c",
+    "url": "/static/css/main.344ecbd1.chunk.css"
+  },
+  {
+    "revision": "baafff6af2dbbc31166b",
+    "url": "/static/js/2.13f7c121.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.13f7c121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "884268b2431702620e6c",
+    "url": "/static/js/main.92223219.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
